3ea65a370c1edef5fff35101fa790d24b330bdde
Andreas Gropp <starmaerker@googlemail.com>
2019-01-15 14:47:24 +0100

    added remaining functions

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ MongoClient.connect(
 
     dboper.insertDocument(db, { name: "Beta", level: 7 }, "elite", result => {
       console.log("Inserted document:\n", result.ops);
+
+      dboper.findDocuments(db, "elite", docs => {
+        console.log("Found documents:\n", docs);
+
+        dboper.updateDocument(
+          db,
+          { name: "Alpha" },
+          { level: 9 },
+          "elite",
+          result => {
+            console.log("Updated document:\n", result.result);
+
+            dboper.findDocuments(db, "elite", docs => {
+              console.log("Found documents:\n", docs);
+
+              db.dropCollection("elite", result => {
+                console.log("Dropped collection: ", result);
+
+                client.close();
+              });
+            });
+          }
+        );
+      });
     });
   }
 );
